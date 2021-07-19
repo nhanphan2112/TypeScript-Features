4e0bea37b86631ef354add29ac8b2009956c1513
Nhan Phan <nphanacc@gmail.com>
2021-07-19 13:02:45 -0700

    343-Void and never function annotation

--- annotations/functions.ts
+++ annotations/functions.ts
@@ -14,3 +14,13 @@ function divide(a: number, b: number): number {
 const multiple = function (a: number, b: number): number {
 	return a * b;
 };
+
+const logger = (message: string): void => {
+	console.log(message);
+};
+
+const throwError = (message: string): void => {
+	if (!message) {
+		throw new Error(message);
+	}
+};
