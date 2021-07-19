2960fae45cc576970b4f2ab1d26190b58d121409
Nhan Phan <nphanacc@gmail.com>
2021-07-19 12:57:53 -0700

    More annotations for function

--- annotations/functions.ts
+++ annotations/functions.ts
@@ -1,3 +1,16 @@
-const add = (a:number, b:number):number => {
- return a+b;
+const add = (a: number, b: number): number => {
+	return a + b;
+};
+
+const subtract = (a: number, b: number): number => {
+	return a - b;
+};
+
+function divide(a: number, b: number): number {
+	return a / b;
 }
+
+//below is an example of anonymous fuction in Typescript
+const multiple = function (a: number, b: number): number {
+	return a * b;
+};
