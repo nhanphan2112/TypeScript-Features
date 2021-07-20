6c5bf05a373d2fab5a1ba466a2c2e09ddae66cd5
Nhan Phan <nphanacc@gmail.com>
2021-07-20 10:53:01 -0700

    355-Using Interface to replace long annotations inside a function argument

--- interfaces.ts
+++ interfaces.ts
@@ -1,14 +1,28 @@
-// Long Type Annotations
+interface Vehicle {
+	name: string;
+	year: number;
+	broken: boolean;
+}
+
 const oldCivic = {
 	name: "civic",
 	year: 2000,
 	broken: true,
 };
 
+// Long Type Annotations
 const printVehicle = (vehicle: { name: string; year: number; broken: boolean }): void => {
 	console.log(`Name: ${vehicle.name}`);
 	console.log(`Year: ${vehicle.year}`);
 	console.log(`Broken: ${vehicle.broken}`);
 };
-
 printVehicle(oldCivic);
+
+// Interface usage replacing long type annoation
+const printVehicleWithInterface = (vehicle: Vehicle): void => {
+	console.log(`Name: ${vehicle.name}`);
+	console.log(`Year: ${vehicle.year}`);
+	console.log(`Broken: ${vehicle.broken}`);
+};
+
+printVehicleWithInterface(oldCivic);
