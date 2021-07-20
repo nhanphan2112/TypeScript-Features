fa175b04997460ce1f08b71f67aff9b8b642807a
Nhan Phan <nphanacc@gmail.com>
2021-07-20 09:57:53 -0700

    351-Tuples in Typescript

--- tuples.ts
+++ tuples.ts
@@ -6,7 +6,7 @@ const drink = {
 
 // Type alias
 type Drink = [string, boolean, number];
-
+// Below are tuples
 const pepsi: Drink = ["brown", true, 40];
 const sprite: Drink = ["clear", true, 40];
 const tea: Drink = ["brown", false, 0];
