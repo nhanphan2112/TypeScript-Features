b9f398ef2b2a93f2a94d125fdcfde20e48624655
Nhan Phan <nphanacc@gmail.com>
2021-07-21 08:55:06 -0700

    357-Fuctions in Interfaces

--- interfaces.ts
+++ interfaces.ts
@@ -1,17 +1,14 @@
-interface Vehicle {
-	name: string;
-	year: Date;
-	broken: boolean;
-  summary(): string;
-}
+interface Reportable {
+	summary(): string;
+} 
 
 const oldCivic = {
 	name: "civic",
 	year: new Date(),
 	broken: true,
-  summary(): string{
-    return `Name: ${this.name}`
-  }
+	summary(): string {
+		return `Name: ${this.name}`;
+	},
 };
 
 // Long Type Annotations
@@ -23,8 +20,8 @@ const printVehicle = (vehicle: { name: string; year: Date; broken: boolean }): v
 printVehicle(oldCivic);
 
 // Interface usage replacing long type annoation
-const printVehicleWithInterface = (vehicle: Vehicle): void => {
-	console.log(vehicle.summary());
+const printSummaryInterface = (item: Reportable): void => {
+	console.log(item.summary());
 };
 
-printVehicleWithInterface(oldCivic);
+printSummaryInterface(oldCivic);
