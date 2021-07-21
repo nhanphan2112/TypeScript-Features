3e5062bed507af040b2431db9968b013872d2dfa
Nhan Phan <nphanacc@gmail.com>
2021-07-21 09:27:56 -0700

    361-Basic Inheritance

--- classes.ts
+++ classes.ts
@@ -3,11 +3,17 @@ class Vehicle {
 		console.log("chugga chugga");
 	}
 
-  honk(): void {
-    console.log('beep');
-  }
+	honk(): void {
+		console.log("beep");
+	}
 }
 
-const vehicle = new Vehicle();
-vehicle.drive();
-vehicle.honk();
+class Car extends Vehicle {
+	drive(): void {
+		console.log("vroom");
+	}
+}
+
+const car = new Car();
+car.drive();
+car.honk();
